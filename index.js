d302314d056859ecfd130b884b9b68916b3129b8
Ethee <jakemmattern@gmail.com>
2020-09-11 20:29:10 -0400

    Add basic calculator functionality

--- index.js
+++ index.js
@@ -1,31 +1,95 @@
+//inits
+let firstOperand = null;
+let secondOperand = null;
+let mathOperator = null;
 const $numbers = document.querySelectorAll('.number');
-const display = document.querySelector('#display');
-const $operators = document.querySelectorAll('.operators');
-$numbers.forEach($numbers => $numbers.addEventListener('click', () => {
-    display.append($numbers.innerHTML);
+const displayNumber = document.querySelector('#displayNumber');
+const displayOperator = document.querySelector('#displayOperator');
+const $operator = document.querySelectorAll('.operator');
+const evalButton = document.querySelector('#evaluate');
+const clearButton = document.querySelector('#clearAll');
+const clearEntryButton = document.querySelector('#clearEntry');
+const decimalButton = document.querySelector('#decimal');
+//clear display init whitespace
+displayNumber.innerText = "0";
+displayOperator.innerText = "";
+
+const numberButtons = $numbers.forEach($numbers => $numbers.addEventListener('click', () => {
+    if(displayNumber.getAttribute('data-count') === "1" && displayOperator.innerText === ""){
+        displayNumber.innerText = ($numbers.innerText);
+        displayNumber.setAttribute('data-count', 0);
+    }
+    else if(displayNumber.innerText.length < 10 | displayOperator.innerText !== ""){
+        if(displayOperator.innerText !== ""){
+            firstOperand = displayNumber.innerText;
+            console.log(firstOperand);
+            mathOperator = displayOperator.innerText;
+            displayNumber.innerText = "";
+            displayOperator.innerText = "";
+        }
+        if(displayNumber.innerText === "0" | isNaN(parseInt(displayNumber.innerText))){
+            displayNumber.innerText = ($numbers.innerText);
+            displayNumber.setAttribute('data-count', 0);
+        }
+        else{
+            displayNumber.innerText += ($numbers.innerText);
+        }
+    }
 }));
-$operators.forEach($operators => $operators.addEventListener('click', () => {
-    let curOperator = display.innerHTML;
-    if(curOperator.charAt(curOperator.length - 1) == /[^0-9]/){
-        display.append($operators.innerHTML);
+const operatorButtons = $operator.forEach($operator => $operator.addEventListener('click', () => {
+    if(firstOperand === null){
+        displayOperator.innerText = $operator.innerText;
     }
     else{
-        
+        secondOperand = displayNumber.innerText;
+        evaluate(firstOperand, secondOperand, mathOperator);
+        mathOperator = $operator.innerText;
     }
 }));
-function add(op1, op2){
-    return op1 + op2;
-}
-function subtract(op1, op2){
-    return op1 - op2;
-}
-function multiply(op1, op2){
-    return op1 * op2;
-}
-function divide(op1, op2){
-    return op1 / op2;
-}
-function evaluate(){
-}
-function operator(){
+const equalsButton = evalButton.addEventListener('click', () => {
+    secondOperand = displayNumber.innerText;
+    evaluate(firstOperand, secondOperand, mathOperator);
+    firstOperand = null;
+});
+const clearAll = clearButton.addEventListener('click', () => {
+    displayNumber.innerText = "0";
+    displayOperator.innerText = "";
+    firstOperand = null;
+    secondOperand = null;
+});
+const clearEntry = clearEntryButton.addEventListener('click', () => {
+    displayNumber.innerText = "0";
+    displayOperator.innerText = "";
+});
+const makeDecimal = decimalButton.addEventListener('click', () => {
+    if(displayNumber.innerText !== /^\d+\.?\d*$/){
+        displayNumber.innerText += ".";
+    }
+});
+function evaluate(num1, num2, operator){
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+    console.log(num1, num2);
+    switch(operator){
+        case '+':
+            secondOperand = num1 + num2;
+            break;
+        case '-':
+            secondOperand = num1 - num2;
+            break;
+        case 'x':
+            secondOperand = num1 * num2;
+            break;
+        case '/':
+            secondOperand = num1 / num2;
+            break;
+    }
+    //if result takes up more than calculator space convert to sci notation
+    if(secondOperand.toString().length > 10){
+        secondOperand = secondOperand.toExponential(7);
+    }
+    displayNumber.innerText = secondOperand;
+    displayOperator.innerText = "";
+    firstOperand = secondOperand;
+    displayNumber.setAttribute('data-count', 1);
 }
